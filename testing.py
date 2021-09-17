ce29a78bb7f471060186ddc84bd05b66fd173d04
evinatn <88366889+evinatn@users.noreply.github.com>
2021-09-17 18:53:53 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options 
+from selenium import webdriver
+from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
@@ -18,76 +22,75 @@ print(" Task 6: Testing(Automated)")
 print(" This program will test the Website Features automatically using Selenium Library.")
 print("\n Website for Testing: "+ driver.current_url)
 print("\n We are going to test 10+ features of this website")
-print("\n\n !!!!!!!!!!!!!!! Cases for Testing !!!!!!!!!!!!!!! ")
+print("\n\n              CASES FOR TESTING              ")
 
 #Case 1: Verification of Title
-print("\n Testing for Case 1: Title Verification...")
+print("\n Case 1: Title Verification...")
 if(driver.title):
-    print(" Title Verification Successfully Completed! ")
+    print(" Verification of Title successful! ")
     print(" Title: "+driver.title)
     count+=1
     
 else:
     print(" Title Verification Failed!")
 
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 
 #Case 2: To find and verify the logo of the webpage
-print("\n Testing for Case 2: Finding and Verification of Logo...")
+print("\n Case 2: Finding and Verification of Logo...")
 try:
     driver.find_element_by_xpath('//*[@id="home"]/div/div[1]/h1/a/*').click()
-    print(" Logo has been found and has been successfully verified!")
+    print("Logo found and verified successfully!")
     time.sleep(4)
     count+=1
     
 except NoSuchElementException:
     print(" No Logo is present! Verification Failed!")
 
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 
 #Case 3: Verification of Navigation Bar
-print("\n Testing for Case 3: Navigation Bar Verfication...")
+print("\n Case 3: Navigation Bar Verfication...")
 try:
     driver.find_element_by_class_name("navbar")
-    print(" Navigation Bar Verification Successfully Completed!")
+    print(" Verification of Navigation Bar Successfully Completed!")
     time.sleep(2)
     count+=1
     
 except NoSuchElementException:
-    print(" Navigation Bar Verification!")
+    print(" Navigation Bar Verification Failed!")
 
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 
 #Case 4: Verification of About Us WebPage
-print("\n Testing for Case 4: About Us WebPage Verification...")
+print("\n Case 4: About Us WebPage Verification...")
 try:
     driver.find_element_by_link_text("About Us").click()
     time.sleep(4)
     driver.find_element_by_link_text("Corporate Partners").click()
     time.sleep(4)
-    print(" About Us WebPage visited and successfully verified!")
+    print(" About Us WebPage visited and verified successfully!")
     count+=1
     
 except NoSuchElementException:
     print(" About Us WebPage Verification Failed! No such webpage exists!")
 
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 #Case 5: Verification of HomePage
-print("\n Testing for Case 5: HomePage Verification...")
+print("\n Case 5: HomePage Verification...")
 try:
     driver.find_element_by_partial_link_text("The Sparks Foundation").click()
     time.sleep(4)
-    print(" HomePage Verification Successfully Completed!")
+    print("  Verification of HomePage Successfully Completed!")
     count+=1
     
 except NoSuchElementException:
     print(" Homepage Verification Failed! HomePage Link doesn't work.")
 
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 
 #Case 6: Verification of Policies and Code WebPage
-print("\n Testing for Case 6: Policies and Code WebPage Verification...")
+print("\n Case 6: Policies and Code WebPage Verification...")
 try:
     driver.find_element_by_link_text("Policies and Code").click()
     time.sleep(4)
@@ -95,16 +98,16 @@ try:
     time.sleep(4)
     driver.find_element_by_link_text("Code of Ethics and Conduct").click()
     time.sleep(4)
-    print(" Policies and Code WebPage Verification Successfully Completed!")
+    print("  Verification of Policies and Code WebPage Successfully Completed!")
     count+=1
     
 except NoSuchElementException:
     print(" Policies and Code WebPage Verification Failed! No such webpage exists!")
     
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 
 #Case 7: Verification of Programs WebPage
-print("\n Testing for Case 7: Programs WebPage Verification...");
+print("\n Case 7: Programs WebPage Verification...");
 try:
     driver.find_element_by_link_text("Programs").click()
     time.sleep(2)
@@ -116,16 +119,16 @@ try:
     time.sleep(4)
     driver.find_element_by_link_text("Workshops").click()
     time.sleep(4)
-    print(" Programs WebPage Verification Successfully Completed!")
+    print("  Verification of Programs WebPage Successfully Completed!")
     count+=1
     
 except NoSuchElementException:
     print(" Programs WebPage Verification Failed! No such webpage exists!")
 
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 
 #Case 8: Verification of LINKS Webpage
-print("\n Testing for Case 8: Links WebPage Verification")
+print("\n Case 8: Links WebPage Verification")
 try:
     driver.find_element_by_link_text("LINKS").click()
     time.sleep(2)
@@ -133,16 +136,16 @@ try:
     time.sleep(4)
     driver.find_element_by_link_text("AI in Education").click()
     time.sleep(4)
-    print(" Links WebPage Verification Successfully Completed!")
+    print("  Verification of Links WebPage Successfully Completed!")
     count+=1
     
 except NoSuchElementException:
     print(" Links Webpage Verification Failed! No such webpage exists!")
 
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 
 #Case 9: Verification of Contact Us WebPage
-print("\n Testing for Case 9: Contact Us WebPage Verification")
+print("\n Case 9: Contact Us WebPage Verification")
 try:
     driver.find_element_by_link_text("Contact Us").click()
     time.sleep(2)
@@ -154,16 +157,16 @@ try:
         print(" Contact Information is Correct!")
     else:
         print(" Contact Information is Incorrect!")
-    print(" Contact Us WebPage Verification Successfully Completed!")
+    print("  Verification of Contact Us WebPage Successfully Completed!")
     count+=1
     
 except NoSuchElementException:
     print(" Contact Us WebPage Verification Failed!")
 
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 
 #Case 10: Verification of Join Us WebPage and Checking the Form Execution:
-print("\n Testing for Case 10: Join Us Page Verification")
+print("\n Case 10: Join Us Page Verification")
 try:
     driver.find_element_by_link_text("Join Us").click()
     time.sleep(2)
@@ -175,7 +178,7 @@ try:
     time.sleep(4)
     select=Select(driver.find_element_by_class_name("form-control"))
     time.sleep(4)
-    select.select_by_visible_text("Web Development Intern")
+    select.select_by_visible_text("Intern")
     time.sleep(4)
     driver.find_element_by_class_name("button-w3layouts").click()
     time.sleep(4)
@@ -185,12 +188,12 @@ try:
 except NoSuchElementException:
     print(" Join Us WebPage Verification Failed!")
 
-print("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n 。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。")
 
 driver.close()
 
 if(count==10):
-    print("The Website has been successfully verified and all its feature is working successfully!!!!!!")
+    print("The Website has been successfully verified and all its features are working as expected!")
     
 else:
-    print("Some of the features of website is not working or not available!!!!")
+    print("The website has some features that do not work or are not available!")
